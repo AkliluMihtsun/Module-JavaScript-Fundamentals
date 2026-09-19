bb906add361126b8b6ca438498945aba093a116d
Aklilu Mihtsun <mihtsun.app@gmail.com>
2026-09-19 16:10:09 +0100

    practicing

---
+++ prep/facts.js
@@ -0,0 +1,1 @@
+console.log("collaborating in Github develops Github skills");
---
+++ prep/greeting.js
@@ -0,0 +1,5 @@
+const greeting = "Hello there";
+let name = "Alicia";
+name = "Aklilu";
+//console.log(`${greeting}, ${name}`);
+console.log(greeting + "," + name);
