2a3a27d91ddcd6e0847775bc6dec302d585642dc
Aklilu Mihtsun <mihtsun.app@gmail.com>
2026-09-26 00:12:27 +0100

    Fix variable redeclaration error in capitalise function

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  I am predicting the code will generate error because the variable str is declared twice
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+console.log(capitalise("hello World"));*/
 
-// =============> write your explanation here
-// =============> write your new code here
+// After we run the code, it generated this error SyntaxError: Identifier str has already been declared
+// This happened because we tried to declare again using the keyword let the variable str
+// which was declared before in the parameter of the function
+
+//Here is the fixed code
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise("hello World"));
