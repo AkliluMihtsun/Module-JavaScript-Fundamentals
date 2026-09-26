47e6f25c0a240865eef34951a247f3134a536ed1
Aklilu Mihtsun <mihtsun.app@gmail.com>
2026-09-26 01:15:09 +0100

    Fix function parameter declaration and update error prediction comments

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,20 +1,21 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// I predict the error will come from the argument because when we write a function we first declare parameters not values
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
+}*/
 
-// =============> write the error message here
+// SyntaxError: Unexpected number
 
-// =============> explain this error message here
+// this error tells us javascript expects valid variable identifiers when function is defined not values
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
-
-
+// Here is the fixed code
+function square(num) {
+  return num * num;
+}
+console.log(square(3));
